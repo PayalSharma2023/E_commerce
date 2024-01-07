25f0d86c9a7b2e287b62d131b89c5101b07868c5
Payal Sharma <payalsharmareal@gmail.com>
2024-01-07 18:34:01 +0530

    updates

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const CustomerRouter = require('./Router/CustomerRoute')
 const app = express()
 
 app.use(express.json())
+
+app.use('/customer', CustomerRouter)
