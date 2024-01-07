cc6c64556b3c99a5708938469f2ac76e7954d6b4
Payal Sharma <payalsharmareal@gmail.com>
2024-01-07 18:39:48 +0530

    updates

--- Model/customer.model.js
+++ Model/customer.model.js
@@ -1,2 +1,19 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
+const Schema = mongoose.Schema({
+    name : {
+        type : String,
+        trim : true,
+        require : true
+    },
+
+    email : {
+        type : String,
+        require : true,
+        validate : (email) => {
+            validator.
+
+        }
+    }
+})
