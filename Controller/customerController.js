2cba994b1c7b96bd37fa9b45e5debd6ee0d63a19
Payal Sharma <payalsharmareal@gmail.com>
2024-01-08 20:17:16 +0530

    customer controller

--- Controller/customerController.js
+++ Controller/customerController.js
@@ -1,7 +1,23 @@
 const { CustomerModel } = require("../Model/customerModel")
 
-const createCustomer = () => {
+const createCustomer = async (req, res) => {
     try {
+        const {name, email, password, address} = req.body
+        if (!name || !email || !password ||!address) {
+            res.status(400).json({
+                message : "please enter name, email, password and address"
+            })
+            return
+        }
+        const customer = await customer.CustomerModel({
+            name : name,
+            email : email,
+            password : password,
+            address : address
+        })
+        res.status(200).json({
+            message : "customer created successfully"
+        })
 
     } catch (err) {
         
