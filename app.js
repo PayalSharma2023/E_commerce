eb345eceb89c1366d17e5d10b63c3ea84d04afde
Payal Sharma <payalsharmareal@gmail.com>
2024-01-09 06:36:18 +0530

    connect to database

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const CustomerRouter = require('./Router/CustomerRoute')
 const app = express()
+require(dotenv).config()
 
 app.use(express.json())
+
+const MongoDb = process.env.MongoDBString
+
+mongoose.connect(MongoDb) 
+    .then((c)=> {
+        app.listen(3003), () => {
+            console.log('connected to database')
+            console.log('server running on port 3003')
+        }
+
+    })
 
 app.use('/customer', CustomerRouter)
