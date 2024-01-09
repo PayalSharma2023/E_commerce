46c44f9037258dc606e883e8008ba46e5e4ff62a
Payal Sharma <payalsharmareal@gmail.com>
2024-01-09 06:07:42 +0530

    login page

--- Controller/customerController.js
+++ Controller/customerController.js
@@ -1,6 +1,8 @@
 const { CustomerModel } = require("../Model/customerModel")
+const bcrypt = require('bcrypt')
 
 const createCustomer = async (req, res) => {
+
     try {
         const {name, email, password, address} = req.body
         if (!name || !email || !password ||!address) {
@@ -9,10 +11,11 @@ const createCustomer = async (req, res) => {
             })
             return
         }
+        hash_password = await bcrypt.hash( password, 8 )
         const customer = await CustomerModel({
             name : name,
             email : email,
-            password : password,
+            password : hash_password,
             address : address
         })
 
@@ -28,4 +31,33 @@ const createCustomer = async (req, res) => {
 
 }
 
-module.exports = {createCustomer}
+const loginCustomer = async (req, res) => {
+    try {
+        const {email, password} = req.body
+        if (!email || !password) {
+            res.status(400).json({message : "please enter email and password"})
+            return
+        }
+        const exsistingCustomer = await CustomerModel.findOne(email)
+        if (!exsistingCustomer) {
+            res.status(404).json({
+                message : "customer not found"
+            })
+            return
+        }
+
+        const passwordMatch = await bcrypt.compare(password, exsistingCustomer.password)
+
+        if (passwordMatch) {
+            res.status(200).json({
+                message : "customer logged in successfully"
+            })
+            return
+        }
+
+    } catch (err) {
+        res.status(500).json({message : "internal server error" + err})
+    }
+}
+
+module.exports = {createCustomer, loginCustomer}
