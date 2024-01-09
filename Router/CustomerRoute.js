186e9df0f517da523ef3ff78c2e41cec89284966
Payal Sharma <payalsharmareal@gmail.com>
2024-01-09 06:23:15 +0530

    added login in customer router

--- Router/CustomerRoute.js
+++ Router/CustomerRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {createCustomer} = require('../Controller/customerController')
+const {createCustomer, loginCustomer} = require('../Controller/customerController')
 
 const router = express.Router()
 
 router.post('/signup', createCustomer)
+router.post('/login', loginCustomer)
 
 module.exports = router
