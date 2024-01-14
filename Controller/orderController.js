9fb398b6b25b512c2ec6379fd0056a848534661a
Payal Sharma <payalsharmareal@gmail.com>
2024-01-14 18:26:47 +0530

    order controller

--- Controller/orderController.js
+++ Controller/orderController.js
@@ -1,10 +1,25 @@
-const CustomerModel = require("../Model/customerModel")
+const { OrderModel } = require("../Model/OrderModel");
+const OrderModel = require("../Model/customerModel")
 
 const OrderPlaced = async (req, res) => {
     const {product, customer, deliveryDate} = req.body;
     if (!product || !customer || !deliveryDate) {
+        res.status(400).json({
+            message : "please enter product , customer,  deliveryDate"
+        })
+        return
     }
     try {
+        const order = await OrderModel({
+            product : product, 
+            customer : customer,
+            deliveryDate : deliveryDate
+        })
+
+        res.status(200).json({
+            messsage : "order placed successfully",
+            order : order._id
+        })
 
     } catch (err) {
         res.status(500).json({
@@ -12,3 +27,5 @@ const OrderPlaced = async (req, res) => {
         })
     }
 }
+
+module.exports = { OrderPlaced }
