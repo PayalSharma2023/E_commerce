3f19e4ba0377659163fdb3eaee3d685be7ae763f
Payal Sharma <payalsharmareal@gmail.com>
2024-01-28 19:33:36 +0530

    updates

--- Router/OrderRoute.js
+++ Router/OrderRoute.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { OrderPlaced } = require('../Controller/orderController')
+const { OrderPlaced, UpdateOrder } = require('../Controller/orderController')
 
 const router = express.Router()
 
-//router.post('/order', OrderPlaced)
+router.post('/order', OrderPlaced)
+router.put('/update_order', UpdateOrder)
 
 module.exports = router
