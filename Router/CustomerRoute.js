7010988b1b672c8cad31294f44ff7b367264bf46
Payal Sharma <payalsharmareal@gmail.com>
2024-02-03 21:44:46 +0530

    customer route

--- Router/CustomerRoute.js
+++ Router/CustomerRoute.js
@@ -1,15 +1,20 @@
 const express = require('express')
-const { getAll, GetProducts, OrderProducts, CancelOrder, addProductToWishlist, removeProductFromWishlist, filterProducts, TrackOrder, customerAuth } = require('../Controller/customerController')
+const {getAll,
+    GetProducts, OrderProducts,CancelOrder, RateProducts,
+    addProductToWishlist, removeProductFromWishlist, filterProducts, TrackOrder,
+    customerAuth
+} = require('../Controller/customerController')
 
 const router = express.Router()
 
 router.get('getAll', getAll)
-//router.get('/get_all_products',customerAuth, GetProducts)
-//router.get('/order_product',customerAuth, OrderProducts)
-//router.delete('/cancel_order', customerAuth, CancelOrder)
-//router.get('/add_to_wishlist', customerAuth, addProductToWishlist)
-// router.delete('/remove_from_wishlist',customerAuth, removeProductFromWishlist)
-//router.get('/filter', filterProducts)
-// router.get('/order_status', TrackOrder)
+router.post('rate', RateProducts)
+router.get('/get_all_products',customerAuth, GetProducts)
+router.get('/order_product',customerAuth, OrderProducts)
+router.delete('/cancel_order', customerAuth, CancelOrder)
+router.get('/add_to_wishlist', customerAuth, addProductToWishlist)
+router.delete('/remove_from_wishlist',customerAuth, removeProductFromWishlist)
+router.get('/filter', filterProducts)
+router.get('/order_status', TrackOrder)
 
 module.exports = router
